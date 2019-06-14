347263313abeddd61532ab453157f06356e34d85
Nematjon <frank.sport.93@gmail.com>
2019-06-14 17:41:46 +0300

    Added context directory

---
+++ context/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+import logo from "./logo.svg"
+
+const Cat = props => {
+  const mouse = props.mouse;
+  return (
+    <img src={logo} alt="Logo" style={{position: "absolute", left: mouse.x, top: mouse.y}} />
+  );
+};
+
+class Mouse extends React.Component {
+  state = {
+    x: 0,
+    y: 0
+  }
+  handleMouseMove = event => {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    })
+  }
+
+  render() {
+    return (
+      <div style={{height: "100%"}} onMouseMove={this.hendleMouseMove}>
+        {this.props.render(this.state)}
+      </div>
+    );
+  }
+}
+const App = (props) => {
+  return (
+    <div className="App">
+      <h1>Move the Mouse around!</h1>
+      <Mouse render={mouse => (
+        <Cat mouse={mouse} />
+      )} />
+    </div>
+  );
+}
+
+export default App;
